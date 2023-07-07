0134e9090877b3d56474a62b19e3e02cc5d5e3c1
Tony <tnp087@gmail.com>
2023-07-07 02:55:42 -0400

    initial imports and routes

--- drive-wise/src/App.jsx
+++ drive-wise/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from './components/Header'
+import CarDetails from './pages/CarDetails'
+import Listings from './pages/Listings'
 
 const App = () => {
 
   return (
     <div className='App'>
-
+      <Header/>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </main>
     </div>
   )
 }
--- drive-wise/src/components/Header.jsx
+++ drive-wise/src/components/Header.jsx
@@ -1,8 +1,15 @@
 const Header = () => {
   return (
-    <div>
+    <header>
+      <nav>
+        <h1 className="nav-header">DriveWise</h1>
+        <div className="header-img">
 
-    </div>
+        </div>
+        <div className="nav-links">
+        </div>
+      </nav>
+    </header>
   )
 
 }
