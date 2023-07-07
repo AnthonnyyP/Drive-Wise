5d8aac8e1a5ebc99406dac0e8f779ccfeeb3044b
Tony <tnp087@gmail.com>
2023-07-07 02:29:16 -0400

    base functions for reach component

---
+++ drive-wise/src/components/VehicleCard.jsx
@@ -0,0 +1,10 @@
+const VehicleCard = () => {
+  return (
+    <div>
+
+    </div>
+  )
+
+}
+
+export default VehicleCard
---
+++ drive-wise/src/pages/Listings.jsx
@@ -0,0 +1,10 @@
+const Listings = () => {
+  return (
+    <div>
+
+    </div>
+  )
+
+}
+
+export default Listings
